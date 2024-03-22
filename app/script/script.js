0e3c0fab2119004721465c0d79d12582f4b1118e
Piotr Brzezinski <piotr.brze95@gmail.com>
2024-03-22 16:35:10 +0100

    dodano menuy mobile

--- app/script/script.js
+++ app/script/script.js
@@ -1,3 +1,45 @@
+const btnOpen = document.querySelector("#btnOpen");
+const btnClose = document.querySelector("#btnClose");
+const media = window.matchMedia("(max-width: 53rem)")
+const topNavMenu = document.querySelector(".topnav__menu");
+const main = document.querySelector('main');
+const html= document.querySelector("html");
+
+function setupTopNav(e){
+  if (e.matches){
+    console.log("mobile")
+topNavMenu.setAttribute("inert", "");
+topNavMenu.style.transition = "none";
+} else{
+  console.log("dektop")
+  topNavMenu.removeAttribute("inert");
+  }
+}
+
+function openMobileMenu(){
+  btnOpen.setAttribute("aria-expanded", "true");
+  topNavMenu.removeAttribute("inert");
+  topNavMenu.removeAttribute("style");
+  main.setAttribute('inert', '');
+  bodyScrollLockUpgrade.disableBodyScroll(body);
+
+}
+
+function closeMobileMenu(){
+  btnOpen.setAttribute("aria-expanded", "false");
+  topNavMenu.setAttribute("inert", "");
+  main.removeAttribute('inert');
+  bodyScrollLockUpgrade.enableBodyScroll(body);
+
+  setTimeout(() =>{
+    topNavMenu.style.transition = "none"
+  }, 200)
+
+}
+
+btnOpen.addEventListener("click", openMobileMenu)
+btnClose.addEventListener("click", closeMobileMenu)
+
 const scrollers = document.querySelectorAll(".logo-wrapper")
 
 if(!window.matchMedia("(prefer-reduced-motion: reduce)").matches){
@@ -21,11 +63,17 @@ function addAnimation(){
 
 async function copyContent() {
     try {
-      await navigator.clipboard.writeText('Hello@melon.studio');
-      console.log('Content copied to clipboard');
+      await navigator.clipboard.writeText("Hello@melon.studio");
+      console.log("Content copied to clipboard");
       /* Resolved - text copied to clipboard successfully */
     } catch (err) {
-      console.error('Failed to copy: ', err);
+      console.error("Failed to copy: ", err);
       /* Rejected - text failed to copy to the clipboard */
     }
   }
+
+  setupTopNav(media);
+
+  media.addEventListener("change", function(e){
+    setupTopNav(e);
+  })
