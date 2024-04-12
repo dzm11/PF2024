002b18b601b54c073ba4a8a9dd1119fc85478aa1
Piotr Brzezinski <piotr.brze95@gmail.com>
2024-04-12 14:06:45 +0200

    new head metatags

--- public/app/script/script.js
+++ public/app/script/script.js
@@ -1,4 +1,25 @@
 ///////////////
+//Barba JS
+///////////////
+
+// barba.init({
+//   transitions: [{
+//     name: 'opacity-transition',
+//     leave(data) {
+//       return gsap.to(data.current.container, {
+//         opacity: 0
+//       });
+//     },
+//     enter(data) {
+//       return gsap.from(data.next.container, {
+//         opacity: 0
+//       });
+//     }
+//   }]
+// });
+
+
+///////////////
 //Mobile Menu
 ///////////////
 
